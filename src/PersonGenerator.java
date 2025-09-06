59d61ecab31de68ee1711abfa9d754270e654b10
Bergerj2 <bergerj2@mail.uc.edu>
2025-09-06 04:20:44 -0400

    fourth commit

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -4,37 +4,44 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PersonGenerator {
     public static void main(String[] args) {
-        ArrayList<String> persons = new ArrayList<>();
-        boolean done = false;
-        String personData;
+        Scanner in = new Scanner(System.in);
+        ArrayList<Person> people = new ArrayList<>();
+        boolean continueInput = false;
 
         do {
-            String id = SafeInput.getNonZeroLenString("Enter ID");
-            String firstName = SafeInput.getNonZeroLenString("Enter First Name");
-            String lastName = SafeInput.ZeroLenString("Enter Last Name");
-            String title = SafeInput.ZeroLenString("Enter Title (Mr., Mrs., Ms., Dr., etc.)");
-            int yearOfBirth = SafeInput.getRangedInt("Enter Year of Birth", 1000, 2025);
+            try {
+                String ID = SafeInput.getNonZeroLenString("Enter the person's ID (6 digits)");
+                String firstName = SafeInput.getNonZeroLenString("Enter the person's first name");
+                String lastName = SafeInput.getNonZeroLenString("Enter the person's last name");
+                String title = SafeInput.getNonZeroLenString("Enter the person's title (e.g., Mr., Ms., Dr.)");
+                int YOB = SafeInput.getRangedInt("Enter the person's year of birth", 1940, 2010);
 
-            personData = String.format("%s, %s, %s, %s, %d", id, firstName, lastName, title, yearOfBirth);
-            persons.add(personData);
+                people.add(new Person(ID, firstName, lastName, title, YOB));
 
-            done = SafeInput.getYNConfirm("Are you done entering data?");
-        } while (!done);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Please try again with a valid year of birth.");
+            }
+            continueInput = SafeInput.getYNConfirm("Do you want to enter another person?");
+        } while (continueInput);
 
-        String fileName = SafeInput.getNonZeroLenString("Enter the filename to save to");
+        String fileName = SafeInput.getNonZeroLenString("Enter the name of the file to save to");
         Path filePath = Paths.get(fileName + ".txt");
 
         try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
-            for (String person : persons) {
-                writer.write(person);
+            for (Person p : people) {
+                writer.write(p.toCSV());
                 writer.newLine();
             }
             System.out.println("Data successfully written to " + filePath.toAbsolutePath());
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            in.close();
         }
     }
 }
